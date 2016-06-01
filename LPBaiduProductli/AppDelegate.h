8467e36113ff5a647ea43d889a43efa08f8c2823
//github.com/pengfei923/carloan.git <lipengfei9169@163.com>
2016-06-01 16:01:36 +0800

    Initial Commit

---
+++ LPBaiduProductli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LPBaiduProductli
+//
+//  Created by hoomsun on 16/6/1.
+//  Copyright © 2016年 hoomsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
